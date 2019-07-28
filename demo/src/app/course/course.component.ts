0b73322f2f2d7d6528255dc8a327251d329150e9
pratimvengurlekar <pratim.995@gmail.com>
2019-07-28 17:02:04 +0530

    Add Component and Inlude Bootstrap String Interpoation

---
+++ demo/src/app/course/course.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course',
+  templateUrl: './course.component.html',
+  styleUrls: ['./course.component.css']
+})
+export class CourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  title = "List of courses";
+
+  getTitle(){
+    return this.title.toUpperCase();
+  }
+
+}
